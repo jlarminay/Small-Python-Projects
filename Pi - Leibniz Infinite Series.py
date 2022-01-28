e808c00ed8fef2a3ac065e7308680197a92b6c78
Josh <josh@ursacreative.ca>
2022-01-27 22:24:16 -0800

    Fix to python 3

--- Pi - Leibniz Infinite Series.py
+++ Pi - Leibniz Infinite Series.py
@@ -13,15 +13,15 @@ pi = 0.0
 p_error = 0.0
 
 # Pre-Test Data
-print "-----Leibniz Infinite Series-----\n"
-print "max: \t\t", max
-print "loop_total: \t", loop_total
-print "x: \t\t", x
-print "real_pi: \t", real_pi
-print "pi: \t\t", pi
-print "p_error: \t", p_error
+print("-----Leibniz Infinite Series-----\n")
+print("max: \t\t", max)
+print("loop_total: \t", loop_total)
+print("x: \t\t", x)
+print("real_pi: \t", real_pi)
+print("pi: \t\t", pi)
+print("p_error: \t", p_error)
 
-raw_input("\n\tNext: LOOP FUNCTION.")
+input("\n\tNext: LOOP FUNCTION.")
 
 # Loop Function
 while pi != real_pi:
@@ -30,18 +30,18 @@ while pi != real_pi:
 	sigma = (4*(((-1)**( x ))/(((2)*( x ))+1)))
 	pi += sigma
 	
-	print "-----Leibniz Infinite Series-----\n"
-	print "Sigma: \t\t", sigma
-	print "Abs (Sigma): \t", (math.fabs(sigma))
-	print "\nActual: \t", real_pi
-	print "Generated: \t", pi
+	print("-----Leibniz Infinite Series-----\n")
+	print("Sigma: \t\t", sigma)
+	print("Abs (Sigma): \t", (math.fabs(sigma)))
+	print("\nActual: \t", real_pi)
+	print("Generated: \t", pi)
 	
 	p_error = math.fabs((real_pi - pi) / real_pi)
 	
-	print "\n% Error: \t", p_error
+	print("\n% Error: \t", p_error)
 	x += 1
 	loop_total += 1
-	print "\nLoop = ", loop_total
+	print("\nLoop = ", loop_total)
 
 # End
-raw_input("\n\nEnter To Exit.")
+input("\n\nEnter To Exit.")
--- Pi - Monte Carlo Method.py
+++ Pi - Monte Carlo Method.py
@@ -16,61 +16,61 @@ pi = 0.0
 p_error = 0.0
 
 # Pre-Test Data
-print "-----Monte Carlo Method-----\n"
-print "max: \t\t", max
-print "loop_total: \t", loop_total
-print "add: \t\t", add
-print "M: \t\t", M
-print "N: \t\t", N
-print "real_pi: \t", real_pi
-print "pi: \t\t", pi
-print "p_error: \t", p_error
+print("-----Monte Carlo Method-----\n")
+print("max: \t\t", max)
+print("loop_total: \t", loop_total)
+print("add: \t\t", add)
+print("M: \t\t", M)
+print("N: \t\t", N)
+print("real_pi: \t", real_pi)
+print("pi: \t\t", pi)
+print("p_error: \t", p_error)
 
-raw_input("\n\tNext: LOOP FUNCTION.")
+input("\n\tNext: LOOP FUNCTION.")
 
 # Loop Function
 while pi != real_pi:
 
 	os.system('cls')
 
-	print "-----Monte Carlo Method-----"
+	print("-----Monte Carlo Method-----")
 	
 	x = random.random()
 	y = random.random()
 
-	print "\n(", x, ",", y, ")"	
+	print("\n(", x, ",", y, ")"	)
 	add = (x**2) + (y**2)
-	print "add: \t", add
+	print("add: \t", add)
 	if add <= 1:
 		M += 1
 		N += 1
-		print "\nM:", M
-		print "N:", N
+		print("\nM:", M)
+		print("N:", N)
 	elif add > 1:
 		N += 1
-		print "\nM:", M
-		print "N:", N
+		print("\nM:", M)
+		print("N:", N)
 	else:
-		raw_input("\n\nERROR DETECTED")
+		input("\n\nERROR DETECTED")
 	
 	loop_total += 1
 	
-	print "\nActual: \t", real_pi
+	print("\nActual: \t", real_pi)
 	pi = (4.0* M) / N
-	print "Generated: \t", pi
+	print("Generated: \t", pi)
 	
 	p_error = math.fabs((real_pi - pi) / real_pi)
 	
-	print "\n% Error: \t", p_error
+	print("\n% Error: \t", p_error)
 	
-	print "\nLoop: \t", loop_total
+	print("\nLoop: \t", loop_total)
 
-print "\n\nTotal M:", M
-print "Total N:", N
+print("\n\nTotal M:", M)
+print("Total N:", N)
 
 pi = (4.0* M) / N
 
-print "\n", pi
+print("\n", pi)
 
 # End
-raw_input("\n\nEnter To Exit.")
+input("\n\nEnter To Exit.")
